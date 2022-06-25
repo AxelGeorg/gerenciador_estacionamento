88ea7a8b4443e9b54909e6fe630821b50ce18472
AxelGeorg <axelgeorg16@gmail.com>
2022-06-25 13:32:54 -0300

    tratando cliente menor de idade

--- mainClientes/views.py
+++ mainClientes/views.py
@@ -1,5 +1,8 @@
+from django.forms import ValidationError
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from datetime import date
+from datetime import datetime
 
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from .models import Cliente
@@ -24,7 +27,7 @@ class ClienteList(ListView):
             clientes = clientes.filter(
                 cpf__icontains=txtCPF)
 
-        return clientes 
+        return clientes
 
 
 class ClienteDetail(DetailView):
@@ -32,10 +35,29 @@ class ClienteDetail(DetailView):
     fields = '__all__'
 
 
+def verificar_data(data):
+    if data:
+        data = datetime.strptime(data, "%d/%m/%Y")
+        today = date.today()
+        if (today.year - data.year - ((today.month, today.day) < (data.month, data.day))) >= 18:
+            return True
+        else:
+            return False
+
+    return True
+
+
 class ClienteCreate(CreateView):
     model = Cliente
     fields = '__all__'
     success_url = reverse_lazy('mainClientes:list')
+
+    def form_valid(self, form):
+        if verificar_data(form.data['data_nascimento']) == False:
+            form.add_error('data_nascimento', ValidationError(
+                f"Cliente não pode ser menor de idade."))
+            return self.render_to_response(self.get_context_data(form=form))
+        return super().form_valid(form)
 
 
 class ClienteUpdate(UpdateView):
